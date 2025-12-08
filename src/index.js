5864d980ffc8be1301a23eb8c6a5e3a608143805
msajidaligik <103204957+msajidaligik@users.noreply.github.com>
2025-12-09 00:16:40 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import { HashRouter, Route, Routes } from "react-router-dom";
 import { App } from "./todo/app";
 import "todomvc-app-css/index.css";
 import "todomvc-common/base.css";
-var x = 5;
+//var x = 5;
+var x = (5);
 render(
     <HashRouter>
         <Routes>
